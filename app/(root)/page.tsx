aeaf6bcf54cc430f482750c9fb2d701ba79595f6
blackholeofuniverse <omb4505@gmail.com>
2025-03-26 23:31:17 +0530

    did so far

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
+import InterviewCard from '@/components/InterviewCard'
 import { Button } from '@/components/ui/button'
+import { dummyInterviews } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -16,18 +18,23 @@ const page = () => {
         </div>
         <Image src={"/robot.png"} alt={"robot-dude"} width={400} height={400} className='max-sm:hidden' />
       </section>
-      
+
       <section className='flex flex-col gap-6 mt-8'>
         <h2>Your Interviews</h2>
         <div className="interviews-section">
-          <p>You haven&apos;t taken any interviews yet</p>
+          {dummyInterviews.map((interview) => (
+            <InterviewCard key={interview.id} {...interview} />
+          ))}
         </div>
       </section>
-      
+
       <section className='flex flex-col gap-6 mt-8'>
         <h2>Take an Interview</h2>
         <div className="interviews-section">
-          <p>There are no interviews available</p>
+          {dummyInterviews.map((interview) => (
+            <InterviewCard key={interview.id} {...interview} />
+          ))}
+          {/* <p>You haven&apos;t taken any interviews yet</p> */}
         </div>
       </section>
     </>

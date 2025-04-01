8886a1e3e2634626c44a98c877d9053c34e985e5
blackholeofuniverse <omb4505@gmail.com>
2025-04-01 22:00:14 +0530

    did so far

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,119 +1,119 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-'use server'
+// /* eslint-disable @typescript-eslint/no-explicit-any */
+// 'use server'
 
-import { auth, db } from "@/firebase/admin";
-import { cookies } from "next/headers";
+// import { auth, db } from "@/firebase/admin";
+// import { cookies } from "next/headers";
 
-const ONE_WEEK = 60 * 60 * 24 * 7
+// const ONE_WEEK = 60 * 60 * 24 * 7
 
-export async function signUp(params: SignUpParams) {
-    const { uid, name, email } = params;
+// export async function signUp(params: SignUpParams) {
+//     const { uid, name, email } = params;
 
-    try {
-        const userRecord = await db.collection('users').doc(uid).get();
-        if (userRecord.exists) {
-            return {
-                success: false,
-                message: 'User already exists. Please sign in instead.'
-            }
+//     try {
+//         const userRecord = await db.collection('users').doc(uid).get();
+//         if (userRecord.exists) {
+//             return {
+//                 success: false,
+//                 message: 'User already exists. Please sign in instead.'
+//             }
 
-        }
+//         }
 
-        await db.collection('users').doc(uid).set({
-            name, email
-        })
+//         await db.collection('users').doc(uid).set({
+//             name, email
+//         })
 
-        return {
-            success: true,
-            message: 'Account created successfully. Please sign in.'
-        }
+//         return {
+//             success: true,
+//             message: 'Account created successfully. Please sign in.'
+//         }
 
-    } catch (error: any) {
-        console.error('Error creating a user', error);
+//     } catch (error: any) {
+//         console.error('Error creating a user', error);
 
-        if (error.code === 'auth/email-already-exists') {
-            return {
-                success: false,
-                message: 'This email is already in use.'
-            }
-        }
-        return {
-            success: false,
-            message: 'Failed to create an account.'
-        }
+//         if (error.code === 'auth/email-already-exists') {
+//             return {
+//                 success: false,
+//                 message: 'This email is already in use.'
+//             }
+//         }
+//         return {
+//             success: false,
+//             message: 'Failed to create an account.'
+//         }
 
 
-    }
-}
+//     }
+// }
 
-export async function signIn(params: SignInParams) {
-    const { email, idToken } = params;
-    try {
-        const userRecord = await auth.getUserByEmail(email);
+// export async function signIn(params: SignInParams) {
+//     const { email, idToken } = params;
+//     try {
+//         const userRecord = await auth.getUserByEmail(email);
 
-        if (!userRecord) {
-            return {
-                success: false,
-                message: 'User does not exists. Create an account instead.'
-            }
-        }
+//         if (!userRecord) {
+//             return {
+//                 success: false,
+//                 message: 'User does not exists. Create an account instead.'
+//             }
+//         }
 
-        await setSessionCookie(idToken);
-    } catch (error) {
-        console.log(error)
-        return {
-            success: false,
-            message: 'Failed to log into an account.'
-        }
-    }
-}
+//         await setSessionCookie(idToken);
+//     } catch (error) {
+//         console.log(error)
+//         return {
+//             success: false,
+//             message: 'Failed to log into an account.'
+//         }
+//     }
+// }
 
-export async function signOut() {
-    const cookieStore = await cookies();
+// export async function signOut() {
+//     const cookieStore = await cookies();
 
-    cookieStore.delete("session");
-}
+//     cookieStore.delete("session");
+// }
 
-export async function setSessionCookie(idToken: string) {
-    const cookieStore = await cookies();
+// export async function setSessionCookie(idToken: string) {
+//     const cookieStore = await cookies();
 
-    const sessioinCookie = await auth.createSessionCookie(idToken, {
-        expiresIn: ONE_WEEK * 1000,
-    })
+//     const sessioinCookie = await auth.createSessionCookie(idToken, {
+//         expiresIn: ONE_WEEK * 1000,
+//     })
 
-    cookieStore.set('session', sessioinCookie, {
-        maxAge: ONE_WEEK,
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        path: '/',
-        sameSite: 'lax'
-    })
-}
+//     cookieStore.set('session', sessioinCookie, {
+//         maxAge: ONE_WEEK,
+//         httpOnly: true,
+//         secure: process.env.NODE_ENV === 'production',
+//         path: '/',
+//         sameSite: 'lax'
+//     })
+// }
 
-export async function getCurrentUser(): Promise<User | null> {
-    const cookieStore = await cookies()
-    const sessioinCookie = cookieStore.get('session')?.value
+// export async function getCurrentUser(): Promise<User | null> {
+//     const cookieStore = await cookies()
+//     const sessioinCookie = cookieStore.get('session')?.value
 
-    if (!sessioinCookie) return null;
+//     if (!sessioinCookie) return null;
 
-    try {
-        const decodedClaims = await auth.verifySessionCookie(sessioinCookie, true);
+//     try {
+//         const decodedClaims = await auth.verifySessionCookie(sessioinCookie, true);
 
-        const userRecord = await db.collection('users').doc(decodedClaims.uid).get();
+//         const userRecord = await db.collection('users').doc(decodedClaims.uid).get();
 
-        if (!userRecord.exists) return null;
+//         if (!userRecord.exists) return null;
 
-        return {
-            ...userRecord.data(),
-            id: userRecord.id,
-        } as User
-    } catch (e) {
-        console.log(e)
-        return null;
-    }
-}
+//         return {
+//             ...userRecord.data(),
+//             id: userRecord.id,
+//         } as User
+//     } catch (e) {
+//         console.log(e)
+//         return null;
+//     }
+// }
 
-export async function isAuthenticated() {
-    const user = await getCurrentUser();
-    return !!user;
-}
+// export async function isAuthenticated() {
+//     const user = await getCurrentUser();
+//     return !!user;
+// }
